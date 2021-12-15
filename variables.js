c19e2680aa6bd399cc9cf816b565472bd0b2877a
Matt Ruder <matt.j.ruder@gmail.com>
2021-12-15 10:36:08 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,23 @@
+var movie = "Walk Hard";
+var isEntertaining = true;
+
+movie = "Star Wars"
+
+console.log(movie);
+
+var num4 = 4;
+var num6 = 6;
+
+var product = num4 * num6;
+
+console.log(product);
+
+var favoriteFoods = ["Cornbread", "Apples", "Pie"];
+
+favoriteFoods.shift();
+
+console.log(favoriteFoods);
+
+var movieCharacters = movie.length;
+
+console.log(movieCharacters);
