a60f3cb7f6d6b7ab3e7cc7c91876be3f803a3647
Matt Ruder <matt.j.ruder@gmail.com>
2021-12-15 10:41:47 -0700

    Add model A

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  name = "Tyrannosaurus Rex",
+  color = "Dark Green",
+  heightInFeet = 20,
+  numberOfTeeth = 100,
+  isHungry = true,
+  hasLongArms == false,
+  animalsEaten = ["rabbit", "bird", "mice"],
+  placesBeen = ["watering hole", "mountain", "cave"]
+}
