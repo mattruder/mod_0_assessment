c8b3bec029cde94b1279baadd4fbe8ea3ea5c213
Matt Ruder <matt.j.ruder@gmail.com>
2021-12-15 10:45:30 -0700

    Add model B

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  isHungry = true,
+  isTired = true,
+  heightInFeet = 5.6,
+  numberOfProjects = 27,
+  appsBuilt = ["bank app", "video game", "timer"],
+  friendsList = ["bradley", "kyle", "sam"],
+  name = "Matt",
+  fullName = "Matthew"
+}
